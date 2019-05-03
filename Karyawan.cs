e69d987f3c50ce42b8d079f902a8a1a881bf1839
dewikartikaa <dwikrtka1818@gmail.com>
2019-05-03 16:01:34 +0700

    responsi

---
+++ Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ledimaniss
+{
+    class Karyawan
+    {
+        public Karyawan()
+        {
+        }
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            if (gajibulanan < 0)
+            {
+                Console.WriteLine("Gaji harus diatas 0!");
+            }
+            else
+            {
+                Nik = nik;
+                Nama = nama;
+                GajiBulanan = gajibulanan;
+            }
+        }
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+    }
+}
